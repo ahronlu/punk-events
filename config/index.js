ff5351ff94777af4de7845368b8e1d02a421ac94
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-05-19 08:55:06 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
 export const PER_PAGE = process.env.NEXT_PUBLIC_PER_PAGE || "1";
